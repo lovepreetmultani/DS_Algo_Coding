af1eb77da5df46459b10d77628e42e2aa0a05bac
lovepreet_multani <lovepreet.multani20@gmail.com>
2021-07-29 17:31:13 +0530

    First commit.

---
+++ Coding_Practice/src/Coding1.java
@@ -0,0 +1,10 @@
+
+public class Coding1 {
+
+	public static void main(String[] args) {
+		  int max_bound=Integer.MAX_VALUE;
+	        int min_bound=Integer.MIN_VALUE;
+	        System.out.println(max_bound+ " "+min_bound);
+	}
+
+}
