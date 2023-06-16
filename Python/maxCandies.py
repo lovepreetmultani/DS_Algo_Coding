0ad4eea382665bb51303c3d7d15e4d8098f70699
lovepreetmultani <lovepreet.multani20@gmail.com>
2023-06-17 00:21:45 +0100

    reverse vowels in string

--- Python/maxCandies.py
+++ Python/maxCandies.py
@@ -1,11 +1,6 @@
 
 class Solution(object):
     def kidsWithCandies(candies, extraCandies):
-        """
-        :type candies: List[int]
-        :type extraCandies: int
-        :rtype: List[bool]
-        """
         maxCandies=max(candies)
         for i in range(len(candies)):
             if candies[i] + extraCandies >= maxCandies:
