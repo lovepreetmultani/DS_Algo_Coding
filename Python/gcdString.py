4b8bf5ab0d420a106b0a6df7417589c63dfe2bf2
lovepreetmultani <lovepreet.multani20@gmail.com>
2023-06-16 13:18:46 +0100

    gcd of strings

---
+++ Python/gcdString.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def gcdOfStrings(str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        if str1==str2:
+            return str1
+        elif len(str2)>len(str1):
+            str1, str2 = str2, str1
+        elif str1[:len(str2)]!= str2:
+            return ""
+        else:
+            return gcdOfStrings(str1[len(str2):],str2)
+    
+    result = gcdOfStrings("ABCABC","ABC")
+    print('gcd of string is:',result)
