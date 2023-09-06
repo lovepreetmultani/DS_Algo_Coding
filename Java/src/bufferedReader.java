2b3cb43d7776ad73a356c8e0bb73faeb3687d904
lovepreetmultani <lovepreet.multani20@gmail.com>
2023-09-06 19:45:01 +0100

    buffer reader

---
+++ Java/src/bufferedReader.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+public class bufferedReader {
+        public static void main(String args[] ) throws Exception {
+            //BufferedReader
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("please enter the size of array");
+            int size = Integer.parseInt(br.readLine());
+
+            int[] array = new int[size];
+
+            // Read the array elements
+            String input = br.readLine();
+            StringTokenizer st = new StringTokenizer(input);
+
+            for(int i = 0; i < size; i++) {
+                array[i] = Integer.parseInt(st.nextToken());
+            }
+                int minElement = findMinimumElement(array);
+                System.out.println("The minimum element in the array is: " + minElement);
+
+
+        }
+    public static int findMinimumElement(int[] arr) {
+        int minElement = arr[0];
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] < minElement) {
+                minElement = arr[i];
+            }
+        }
+
+        return minElement;
+    }
+
+}
+
+
+
+
+
