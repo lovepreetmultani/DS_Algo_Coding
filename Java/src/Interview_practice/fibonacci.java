073b34cdb2cce8ad94348295644861eb70f1df74
lovepreetmultani <lovepreet.multani20@gmail.com>
2023-10-09 23:20:19 +0100

    adding

---
+++ Java/src/Interview_practice/fibonacci.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class fibonacci {
+
+    public static List<Integer> fibonacci(int n){
+        List<Integer> list = new ArrayList<>();
+        int firstElement =0;
+        int secondElement=1;
+        int sum=0;
+        System.out.println(firstElement+ " " +secondElement)
+        for (int i = 2;i<n;i++){
+            sum=firstElement+secondElement;
+            firstElement=secondElement;
+            secondElement=sum;
+            list.add(sum);
+        }
+        return list;
+    }
+    public static void main(String args[]){
+        int n = 10;
+        System.out.println("series is:"+fibonacci(n));
+    }
+}
