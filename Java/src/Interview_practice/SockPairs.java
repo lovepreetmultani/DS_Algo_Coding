28fa90d98eec721ddb797f1d118fed8687185b60
lovepreetmultani <lovepreet.multani20@gmail.com>
2023-11-03 15:01:37 +0000

    sock pairs

---
+++ Java/src/Interview_practice/SockPairs.java
@@ -0,0 +1,42 @@
+package src.Interview_practice;
+
+import java.util.HashMap;
+
+public class SockPairs {
+
+    public static void main(String args[]){
+        int[] inputArray= {10, 20, 20, 10, 10, 30, 50, 10, 20};  //{1,2,1,2,1,3,2}
+        int n = inputArray.length;
+
+        System.out.println("pair of socks are: "+sockPairs(inputArray,n));
+    }
+
+    private static int sockPairs(int[] inputArray, int n) {
+            if(n<1){
+            return 0;
+        }
+            int countPairs=0;
+            HashMap<Integer, Integer> mapInput = new HashMap<>();
+            for(int i =0;i<n;i++){
+                if(mapInput.containsKey(inputArray[i]))
+                {
+                    mapInput.put(inputArray[i], mapInput.getOrDefault(inputArray[i],0)+1);
+                }else
+                {
+                    mapInput.put(inputArray[i],1);
+                }
+            }
+
+           for(int j: mapInput.values()){
+                if(j>1){
+                    if(j%2==0){
+                        countPairs++;
+                    }
+                    countPairs++;
+                }
+            }
+
+
+        return countPairs;
+    }
+}
