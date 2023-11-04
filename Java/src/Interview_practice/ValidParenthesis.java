51c72352155fa6b7237c3625387f843fa5149f6b
lovepreetmultani <lovepreet.multani20@gmail.com>
2023-11-04 10:34:36 +0000

    balanced parenthesis

---
+++ Java/src/Interview_practice/ValidParenthesis.java
@@ -0,0 +1,30 @@
+package src.Interview_practice;
+
+import java.util.Stack;
+
+public class ValidParenthesis {
+
+    private static boolean isValid(String str) {
+        if (str.length() < 1) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (char ch : str.toCharArray()) {
+            if (ch == '{') {
+                stack.push('}');
+            } else if (ch == '[') {
+                stack.push(']');
+            } else if (ch == '(') {
+                stack.push(')');
+            }else if(stack.isEmpty() || stack.pop()!=ch){
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String args[]){
+        String str= "()";
+        System.out.println("String is balanced: "+isValid(str));
+    }
+}
