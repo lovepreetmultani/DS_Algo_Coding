ac8cde37b690af2ad5297dacd70e0922e4b4782f
lovepreetmultani <lovepreet.multani20@gmail.com>
2023-11-07 14:30:52 +0000

    updating

--- Java/src/MissingfirstPositive.java
+++ Java/src/MissingfirstPositive.java
@@ -8,19 +8,19 @@ public class MissingfirstPositive {
     }
 
     private static int missingFirstPositive(int[] nums) {
-        int ans[] = new int[nums.length + 1];
+        int array[] = new int[nums.length + 1];
 
         for(int i = 0; i < nums.length; i++){
             int val = nums[i];
             if(val > 0 && val <= nums.length) {
-                ans[val] = 1;
+                array[val] = 1;
             }
         }
-        for(int i = 1; i < ans.length; i++){
-            if(ans[i] == 0){
+        for(int i = 1; i < array.length; i++){
+            if(array[i] == 0){
                 return i;
             }
         }
-        return ans.length;
+        return array.length;
     }
 }
