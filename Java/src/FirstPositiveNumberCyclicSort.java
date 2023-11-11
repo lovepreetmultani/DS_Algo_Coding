c5bf074bcd75b146513cf9891829693fa36a849d
lovepreetmultani <lovepreet.multani20@gmail.com>
2023-11-11 10:04:58 +0000

    update

--- Java/src/FirstPositiveNumberCyclicSort.java
+++ Java/src/FirstPositiveNumberCyclicSort.java
@@ -3,22 +3,22 @@ package src;
 class FirstPositiveNumberCyclicSort {
     public static int missingFirstPositiveCyclicSort(int[] array) {
 
-        int length = array.length;
+        int n = array.length;
 
-        for(int i=0;i<length;i++){
-            while(array[i]>0 && array[i]<=length && array[array[i]-1]!=array[i]){
+        for(int i=0;i<n;i++){
+            while(array[i]>0 && array[i]<=n && array[array[i]-1]!=array[i]){
                 int temp = array[i];
                 array[i]=array[temp-1];
                 array[temp-1]=temp;
             }
         }
 
-        for(int i=0;i<length;i++){
+        for(int i=0;i<n;i++){
             if(array[i]!=i+1){
                 return i+1;
             }
         }
-        return length+1;
+        return n+1;
     }
     public static void main(String s[]){
         int[] num = {1,2,0};
