fb6fde847515f283aeb9754e9cadfe4c94ffc671
lovepreetmultani <lovepreet.multani20@gmail.com>
2023-11-14 15:38:38 +0000

    string concepts

---
+++ Java/src/Pangram.java
@@ -0,0 +1,37 @@
+package src;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Pangram {
+
+    public static void main(String s[]){
+        String input = "The quick brown fox jumps over the dog";//"The quick brown fox jumps over the lazy dog";
+        String inputHash = input.toLowerCase().replaceAll("[^a-z]","");
+        Set<Character> presentElements = new HashSet<>();
+        for(char ch:inputHash.toCharArray()){
+            presentElements.add(ch);
+        }
+        if (isPangram(presentElements)) {
+            System.out.println("The input is a pangram.");
+        } else {
+            String missingChars = getMissingCharacters(presentElements,input);
+            System.out.println("The input is not a pangram. Missing characters: " + missingChars);
+        }
+    }
+
+    private static String getMissingCharacters(Set<Character> input, String s) {
+        StringBuilder sb = new StringBuilder();
+        for(char ch='a';ch<='z';ch++){
+            if(!input.contains(ch)){
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+
+    private static boolean isPangram(Set<Character> input) {
+
+        return input.size()==26;
+    }
+}
---
+++ Java/src/StringConcepts.java
@@ -0,0 +1,9 @@
+package src;
+
+public class StringConcepts {
+    public static void main(String s[]){
+        String s1="abc";
+        s1.concat("def");
+        System.out.println(s1);
+    }
+}
