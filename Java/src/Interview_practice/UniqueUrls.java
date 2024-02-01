f8af9edb810912b45f139050194c6bb6408fd5c7
lovepreetmultani <lovepreet.multani20@gmail.com>
2024-02-01 23:43:19 +0000

    update string

--- Java/src/Interview_practice/UniqueUrls.java
+++ Java/src/Interview_practice/UniqueUrls.java
@@ -16,7 +16,7 @@ import java.net.URISyntaxException;
                     }
                     uniqueUrls.add(normalizedUrl);
                 } catch (URISyntaxException e) {
-                    throw new IllegalArgumentException( "Not a valid URI" );
+                    throw new IllegalArgumentException( "Not a valid URL" );
                 }
             }
             return uniqueUrls.size();
