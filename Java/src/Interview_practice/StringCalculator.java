5945b1c2b23c5b43fcc7e1dfbdb2a9e6ae9a5316
lovepreetmultani <lovepreet.multani20@gmail.com>
2024-02-04 00:30:37 +0000

    string calculator update code

--- Java/src/Interview_practice/StringCalculator.java
+++ Java/src/Interview_practice/StringCalculator.java
@@ -1,18 +1,50 @@
 package src.Interview_practice;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 
-    private static int add(String number) {
-
-        if (number.isEmpty()) {
+    private static int add(String numbers) {
+        if (numbers.isEmpty()) {
             return 0;
         }
 
-       String[] nums=number.split(",");
-       int sum=0;
+        Pattern pattern = Pattern.compile("//\\[(.*?)\\]\n(.*)");
+        Matcher matcher = pattern.matcher(numbers);
+
+        List<String> delimiters = new ArrayList<>();
+        String inputNumbers = numbers;
+
+        while (matcher.find()) {
+            String delimiter = matcher.group(1);
+            delimiters.add(Pattern.quote(delimiter));
+            inputNumbers = matcher.group(2);
+        }
+
+        if (delimiters.isEmpty()) {
+            delimiters.add(",");
+            delimiters.add("\n");
+        }
+
+        String delimiterPattern = String.join("|", delimiters);
+
+        String[] nums = inputNumbers.split(delimiterPattern);
+        int sum=0;
+        List<Integer> negativeNumbers = new ArrayList<>();
        for(String num :nums){
-           sum+=Integer.parseInt(num);
+           int n = Integer.parseInt(num);
+           if(n<0){
+               negativeNumbers.add(n);
+           }else if(n<=1000){
+               sum+=n;
+           }
        }
+        if (!negativeNumbers.isEmpty()) {
+            throw new IllegalArgumentException("Negatives not allowed: " + negativeNumbers);
+        }
        return sum;
     }
 
@@ -21,7 +53,11 @@ public class StringCalculator {
         System.out.println(add(""));
         System.out.println(add("1"));
         System.out.println(add("1,2"));
+        System.out.println(add("1\n2,3"));
+        //System.out.println(add("1,2,-3,4,-5"));
+        System.out.println(add("1001,2"));
+        System.out.println(add("//[|||||||]\n1|||||||2|||||||3"));
+        //System.out.println(add("//[|][%]\n1|2%3"));
+
     }
-
-
 }
