4de51ceb7b3f75a56549ac55be996ee6b0cd76c9
lovepreetmultani <lovepreet.multani20@gmail.com>
2024-02-22 22:48:04 +0000

    rest api web scoket

---
+++ Java/src/Interview_practice/RestConsumeWebSocket.java
@@ -0,0 +1,18 @@
+package src.Interview_practice;
+
+public class RestConsumeWebSocket {
+    // Example of consuming REST API using RestTemplate
+    RestTemplate restTemplate = new RestTemplate();
+    ResponseEntity<String> response = restTemplate.getForEntity("http://api.example.com/data", String.class);
+    String responseBody = response.getBody();
+
+    // Example of WebSocket communication using Spring WebFlux
+    WebSocketClient client = new ReactorNettyWebSocketClient();
+client.execute(URI.create("ws://example.com/socket"), session ->
+        session.send(Mono.just(session.textMessage("Hello")))
+        .thenMany(session.receive()
+        .map(WebSocketMessage::getPayloadAsText)
+        .log())
+        .then())
+        .block(Duration.ofSeconds(10));
+}
