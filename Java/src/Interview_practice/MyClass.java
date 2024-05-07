437cf362cc685af0ca0798f23ef8fbe965b9bbf4
lovepreetmultani <lovepreet.multani20@gmail.com>
2024-05-07 22:34:36 +0100

    removce imports

--- Java/src/Interview_practice/MyClass.java
+++ Java/src/Interview_practice/MyClass.java
@@ -4,11 +4,9 @@ public class MyClass {
     public void myMethod(Object obj) {
         System.out.println("Object method called");
     }
-
     public void myMethod(String str) {
         System.out.println("String method called");
     }
-
     public static void main(String[] args) {
         MyClass obj = new MyClass();
         obj.myMethod(null);
