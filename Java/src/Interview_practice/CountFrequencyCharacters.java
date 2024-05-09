4130a1e260b570366f39509675321eebcd1464c4
lovepreetmultani <lovepreet.multani20@gmail.com>
2024-05-09 19:37:50 +0100

    removce imports

--- Java/src/Interview_practice/CountFrequencyCharacters.java
+++ Java/src/Interview_practice/CountFrequencyCharacters.java
@@ -4,12 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CountFrequencyCharacters {
-
     public static void main(String s[]){
         String str="hello world";
         System.out.println("count and character:"+firstRepeatingCharacter(str));
     }
-
     private static String firstRepeatingCharacter(String str) {
         if(str.length()<1 || str==null){
             return "No element";
@@ -20,14 +18,12 @@ public class CountFrequencyCharacters {
         HashMap<Character, Integer> repeatedChars= new HashMap<Character,Integer>();
 
         for(int i =0;i<str.length();i++){
-
             if(repeatedChars.containsKey(str.charAt(i))){
                 repeatedChars.put(str.charAt(i), repeatedChars.get(str.charAt(i)+1));
             }else{
             repeatedChars.put(str.charAt(i),1);
             }
         }
-
         for(Map.Entry<Character, Integer> entry: repeatedChars.entrySet()){
             int count =entry.getValue();
             System.out.println("values are"+count+"   "+entry.getKey());
