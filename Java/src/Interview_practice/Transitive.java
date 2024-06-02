46610ddc904d25287e7e09d720d1f27c4f1f326d
lovepreetmultani <lovepreet.multani20@gmail.com>
2024-06-02 23:00:32 +0100

    refactoring code

--- Java/src/Interview_practice/Transitive.java
+++ Java/src/Interview_practice/Transitive.java
@@ -13,26 +13,19 @@ public class Transitive {
         int result = transitiveBuysCount(initialCount, buys);
         System.out.println("Transitive buys count: " + result);
     }
-
     public static int transitiveBuysCount(int initialCount, char[][] buys) {
         Map<Character, Set<Character>> buyMap = new HashMap<>();
-
         for (char[] buy : buys) {
             buyMap.computeIfAbsent(buy[0], k -> new HashSet<>()).add(buy[1]);
         }
-
         Set<Character> uniqueBuys = new HashSet<>();
-
         dfs('A', buyMap, uniqueBuys, new HashSet<>());
-
         return uniqueBuys.size()-1;
     }
 
     public static void dfs(char current, Map<Character, Set<Character>> buyMap, Set<Character> uniqueBuys, Set<Character> visited) {
         if (!buyMap.containsKey(current) || visited.contains(current)) return;
-
         visited.add(current);
-
         for (char next : buyMap.get(current)) {
             uniqueBuys.add(next);
             dfs(next, buyMap, uniqueBuys, visited);
