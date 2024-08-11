dbbda20aed77217b275a6168c9bbe37d5b7d0e45
lovepreetmultani <lovepreet.multani20@gmail.com>
2024-08-11 22:33:45 +0100

    Java time API for second friday of next month

---
+++ Java/src/Interview_practice/secondFriday.java
@@ -0,0 +1,16 @@
+package src.Interview_practice;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+public class secondFriday {
+    public static void main(String s[]){
+        var currentDate = LocalDate.now();
+        System.out.println("current date:"+currentDate);
+        var firstDayOfMonth = currentDate.plusMonths(1).withDayOfMonth(1);
+        var firstFriday = firstDayOfMonth.with(TemporalAdjusters.nextOrSame(DayOfWeek.FRIDAY));
+        var secondFriday = firstFriday.plusWeeks(1);
+        System.out.println("second Friday is = "+secondFriday);
+    }
+}
