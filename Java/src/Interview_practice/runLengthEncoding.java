7d0d21b77f15217963e050bb75283de15ab997d2
lovepreetmultani <lovepreet.multani20@gmail.com>
2024-08-11 22:45:30 +0100

    Java time API for second friday of next month

---
+++ Java/src/Interview_practice/runLengthEncoding.java
@@ -0,0 +1,29 @@
+package src.Interview_practice;
+
+public class runLengthEncoding {
+    static String findFrequency(String str) {
+        if(str.length() < 1 || str.isEmpty()){
+            return "";
+        }
+        var count =1;
+        var sb = new StringBuilder();
+        for(var i =0;i<str.length()-1;i++){
+            if(str.charAt(i) == str.charAt(i+1)){
+                count++;
+            }else{
+                sb.append(str.charAt(i));
+                sb.append(count);
+                count=1;
+            }
+        }
+        sb.append(str.charAt(str.length()-1)).append(count);
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "aaabbcc";
+        String str1 = "aaa";
+        System.out.println("frequency is:"+findFrequency(str));
+        System.out.println("frequency 2 is:"+findFrequency(str1));
+    }
+}
