61dd5c73ceecc57cc818661e9dfbd7da533d3354
lovepreetmultani <lovepreet.multani20@gmail.com>
2024-08-22 22:39:57 +0100

    median of sorted array

--- Java/src/BufferedReader.java
+++ Java/src/BufferedReader.java
@@ -5,23 +5,22 @@ import java.util.*;
 public class BufferedReader {
         public static void main(String args[] ) throws Exception {
             java.io.BufferedReader br = new java.io.BufferedReader(new InputStreamReader(System.in));
-            System.out.println("please enter the size of array");
-            int size = Integer.parseInt(br.readLine());
+            var size = Integer.parseInt(br.readLine());
 
-            int[] array = new int[size];
-            String input = br.readLine();
-            StringTokenizer st = new StringTokenizer(input);
+            var array = new int[size];
+            var input = br.readLine();
+            var st = new StringTokenizer(input);
 
             for(int i = 0; i < size; i++) {
                 array[i] = Integer.parseInt(st.nextToken());
             }
-                int minElement = findMinimumElement(array);
+                var minElement = findMinimumElement(array);
                 System.out.println("The minimum element in the array is: " + minElement);
 
 
         }
     public static int findMinimumElement(int[] arr) {
-        int minElement = arr[0];
+        var minElement = arr[0];
         for(int i = 1; i < arr.length; i++) {
             if(arr[i] < minElement) {
                 minElement = arr[i];
