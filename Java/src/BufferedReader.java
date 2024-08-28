ddcb889b2de80025ee6dca6e58e012823c1e6d0e
lovepreetmultani <lovepreet.multani20@gmail.com>
2024-08-28 21:43:42 +0100

    update library

--- Java/src/BufferedReader.java
+++ Java/src/BufferedReader.java
@@ -4,9 +4,8 @@ import java.io.InputStreamReader;
 import java.util.*;
 public class BufferedReader {
         public static void main(String args[] ) throws Exception {
-            java.io.BufferedReader br = new java.io.BufferedReader(new InputStreamReader(System.in));
+            var br = new java.io.BufferedReader(new InputStreamReader(System.in));
             var size = Integer.parseInt(br.readLine());
-
             var array = new int[size];
             var input = br.readLine();
             var st = new StringTokenizer(input);
