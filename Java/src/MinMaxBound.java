3ae7f9a8e99b31c22c1d9ce273650ae9fde363f1
lovepreetmultani <lovepreet.multani20@gmail.com>
2024-08-30 23:32:39 +0100

    refactor

--- Java/src/MinMaxBound.java
+++ Java/src/MinMaxBound.java
@@ -2,10 +2,9 @@
 public class MinMaxBound {
 
 	public static void main(String[] args) {
-		  int max_bound=Integer.MAX_VALUE;
-	        int min_bound=Integer.MIN_VALUE;
+		  var max_bound=Integer.MAX_VALUE;
+	        var min_bound=Integer.MIN_VALUE;
 	        System.out.println(max_bound+ " "+min_bound);
-	        System.out.println("testing additional changes");
 	}
 
 }
