b1947c4f54d8ce4d66d11be9932c9943d0c6bd8e
lovepreetmultani <lovepreet.multani20@gmail.com>
2024-09-15 23:46:06 +0100

    updating java 17

--- Java/src/CountDuplicates.java
+++ Java/src/CountDuplicates.java
@@ -3,12 +3,10 @@ package src;
 import java.util.HashMap;
 
 public class CountDuplicates {
-
     public static void main(String[] args) {
         var str  = "aabbcde";
         System.out.println("duplicate count is: " + countDuplicates(str));
     }
-
     private static int countDuplicates(String str) {
         if(str.length()<0){
             return 0;
@@ -20,8 +18,7 @@ public class CountDuplicates {
                 hm.put(c,hm.getOrDefault(c,0)+1);
             }
         }
-
-        int duplicateCount=0;
+        var duplicateCount=0;
         for(var count: hm.values()){
             if(count>1){
                 duplicateCount++;
