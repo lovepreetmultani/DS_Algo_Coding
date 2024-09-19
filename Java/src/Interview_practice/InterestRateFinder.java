a71b2a103a231cf8985bdeb566af67c6bbdb586e
lovepreetmultani <lovepreet.multani20@gmail.com>
2024-09-19 19:35:23 +0100

    polymorphism overide

--- Java/src/Interview_practice/InterestRateFinder.java
+++ Java/src/Interview_practice/InterestRateFinder.java
@@ -11,11 +11,15 @@ class CitiBank extends Bank {
     public Float getInterestRate() {
         return 5.0f;
     }
+    public Float getParentInterestRate() {
+        return super.getInterestRate();
+    }
 }
 
 public class InterestRateFinder {
     public static void main(String[] args) {
-        var citiBank = new CitiBank();
-        System.out.println("Interest Rate: " + citiBank.getInterestRate());
+        var bank = new CitiBank();
+        System.out.println("Interest Rate: " + bank.getInterestRate());
+        System.out.println("Bank Interest Rate via CitiBank: " + bank.getParentInterestRate());
     }
 }
